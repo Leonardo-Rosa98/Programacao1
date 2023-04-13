d88046c27ea1acb44422eed35e666116159f35a2
Leonardo Rosa <leorosawork@gmail.com>
2023-04-13 11:46:35 -0300

    Meus estudos

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programacao-1
+Esse repositório tem o objetivo de guardar os códigos do meu aprendizado na linguagem python.
---
+++ print_variaveis.py
@@ -0,0 +1,16 @@
+#Utizando função Print
+print("Leonardo", "Rosa", sep="-")
+print("Brasil", "ganhou", 5, "titulos mundiais", end="")
+print(">>>>>")
+alteracao = 5
+
+#Tipos de dados em python:
+
+inteiro = 1 #int
+Flutuante = 2.5 #float
+string = "casa" #str
+lista = [1,2,3] #list
+tupla = (1,2,3) #tuple - valores imutaveis
+dicionario = {1,2,3} #dict
+boleano = True #bool 
+
